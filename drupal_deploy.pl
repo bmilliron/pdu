0dbfe77a4380d1d1a867504034acdbddc4bda676
Ben Milliron <bmilliron@protonmail.com>
2021-01-02 13:53:26 -0500

    deploy drupal

---
+++ drupal_deploy.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+#Deploy stand alone Drupal installation
+
+use IO::Prompt;
+
+my $deployDirectory = "/var/www/html/";
+my $drupalDistArchiveDir = "~/Downloads";
+my $drupalDistFile = "";
+
+#unarchive core install
+system("tar -xzf $drupalDistArchive/$drupalDistFile");
+
+
+
+
+
+
